8344ea19dab3ec6c45f3dd823af0a243dd461f2f
Marco Tulio <marcofc6@gmail.com>
2023-01-16 18:32:14 -0300

    Removed contatos + sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -8,13 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Luiz Otavio',
     })
-
-
-def contato(request):
-    # return HTTP RESPONSE
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    # return HTTP RESPONSE
-    return HttpResponse('SOBRE')
