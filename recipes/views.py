38d6c06ff7e6fa05e411675bfaab411999e64cf9
Marco Tulio <marcofc6@gmail.com>
2023-01-16 13:59:29 -0300

    more changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+# from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    # return HTTP RESPONSE
+    return HttpResponse('HOME')
+
+
+def contato(request):
+    # return HTTP RESPONSE
+    return HttpResponse('CONTATO')
+
+
+def sobre(request):
+    # return HTTP RESPONSE
+    return HttpResponse('SOBRE')
