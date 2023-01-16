c306b46885c892113143e0bb4fa63d5bc121ecf2
Marco Tulio <marcofc6@gmail.com>
2023-01-16 16:02:34 -0300

    render + templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,18 @@
 from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    # return HTTP RESPONSE
-    return HttpResponse('HOME')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Luiz Otavio',
+    })
 
 
 def contato(request):
     # return HTTP RESPONSE
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/contato.html')
 
 
 def sobre(request):
